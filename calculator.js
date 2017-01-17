85b7ff72df8ccd6254a21ba3f2d5f24ae4e16060
Nathan Lewis <mac6@129mac18.valleychristian.net>
2017-01-17 10:54:26 -0800

    0 as default number
    made 0 the default number in the calculator screen

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
 // JavaScript Document
 
 // Change Display
-funtion d(val)  {
+function d(val) {
 	document.getElementById("d").value = val;
 }
 
 // Type numbers and operators
-funtction v(val) {
-		document.getElementById("d").value += val;
+function v(val) {
+	document.getElementById("d").value += val;
 }
 
-// Evaluate the expression
+// Evaluate the equation
 function e() {
-		try {
-			  d(eval(document.getElementById("d").value));
-		}
-		catch(err) {
-				d("Error");
-		}
+	try {
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");
+	}
 }
